86db0b0d3562be1265760254674839fcf52eb2eb
Daniel Blanco Parla <dblancoparla@gmail.com>
2018-05-26 18:43:11 +0200

    catched broken txs

--- src/protocol/serializer.js
+++ src/protocol/serializer.js
@@ -5,57 +5,61 @@ const { TransferContract } = require('../protocol/core/Contract_pb');
 
 
 function deserializeTransaction(tx) {
-  const contractType = Transaction.Contract.ContractType;
+  try {
+    const contractType = Transaction.Contract.ContractType;
 
-  const contractList = tx.getRawData().getContractList();
+    const contractList = tx.getRawData().getContractList();
 
-  const transactions = [];
+    const transactions = [];
 
-  contractList.forEach((contract) => {
-    const any = contract.getParameter();
+    contractList.forEach((contract) => {
+      const any = contract.getParameter();
 
-    switch (contract.getType()) {
-      case contractType.ACCOUNTCREATECONTRACT: {
+      switch (contract.getType()) {
+        case contractType.ACCOUNTCREATECONTRACT: {
         // contractType = contractType .ACCOUNTCREATECONTRACT;
 
-        const obje = any.unpack(AccountCreateContract.deserializeBinary, 'protocol.AccountCreateContract');
+          const obje = any.unpack(AccountCreateContract.deserializeBinary, 'protocol.AccountCreateContract');
 
-        transactions.push({});
-      }
-        break;
-        
-      default:
-      case contractType.TRANSFERCONTRACT: {
+          transactions.push({});
+        }
+          break;
+
+        default:
+        case contractType.TRANSFERCONTRACT: {
         // let contractType = contractType .TRANSFERCONTRACT;
 
-        const obje = any.unpack(TransferContract.deserializeBinary, 'protocol.TransferContract');
+          const obje = any.unpack(TransferContract.deserializeBinary, 'protocol.TransferContract');
 
-        const owner = obje.getOwnerAddress();
-        const ownerHex = getBase58CheckAddress(Array.from(owner));
+          const owner = obje.getOwnerAddress();
+          const ownerHex = getBase58CheckAddress(Array.from(owner));
 
-        const to = obje.getToAddress();
-        const toHex = getBase58CheckAddress(Array.from(to));
+          const to = obje.getToAddress();
+          const toHex = getBase58CheckAddress(Array.from(to));
 
-        const amount = obje.getAmount() / 1000000;
-        
-        const rawData = tx.getRawData();
-        const hash = byteArray2hexStr(SHA256(rawData.serializeBinary()));
+          const amount = obje.getAmount() / 1000000;
 
-        transactions.push({
-          hash,
-          from: ownerHex,
-          to: toHex,
-          amount,
-          time: tx.getRawData().getTimestamp(),
-          data: rawData.getData(),
-          scripts: rawData.getScripts(),
-        });
+          const rawData = tx.getRawData();
+          const hash = byteArray2hexStr(SHA256(rawData.serializeBinary()));
+
+          transactions.push({
+            hash,
+            from: ownerHex,
+            to: toHex,
+            amount,
+            time: tx.getRawData().getTimestamp(),
+            data: rawData.getData(),
+            scripts: rawData.getScripts(),
+          });
+        }
+          break;
       }
-        break;
-    }
-  });
+    });
 
-  return transactions;
+    return transactions;
+  } catch (err) {
+    return [null];
+  }
 }
 
 module.exports = {
