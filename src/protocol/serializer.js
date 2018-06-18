776180a8f3a47cfdcb4b96473b8245cf5a59fdd8
Angel Quiroga <qmangel@gmail.com>
2018-06-18 20:01:37 +0200

    + Added all current transaction serialization.

--- src/protocol/serializer.js
+++ src/protocol/serializer.js
@@ -1,59 +1,61 @@
 const { getBase58CheckAddress, SHA256 } = require('../utils/crypto');
 const { byteArray2hexStr } = require('../utils/bytes');
 const { Transaction } = require('../protocol/core/Tron_pb');
-const { TransferContract } = require('../protocol/core/Contract_pb');
+const {
+  AccountCreateContract,//To delete in a future?
+  TransferContract,
+  TransferAssetContract,
+  VoteAssetContract,
+  VoteWitnessContract, 
+  AssetIssueContract,
+  DeployContract,
+  WitnessUpdateContract,
+  ParticipateAssetIssueContract,
+  FreezeBalanceContract, 
+  UnfreezeBalanceContract ,
+  WithdrawBalanceContract,
+  UnfreezeAssetContract,
+  UpdateAssetContract,
+  CustomContract
+} = require('../protocol/core/Contract_pb');
 
+const ContractType =  Transaction.Contract.ContractType;
+const ContractTable = {};
+ContractTable[ContractType.ACCOUNTCREATECONTRACT] =           [AccountCreateContract.deserializeBinary,'protocol.AccountCreateContract']
+ContractTable[ContractType.TRANSFERCONTRACT] =                [TransferContract.deserializeBinary,'protocol.TransferContract']
+ContractTable[ContractType.TRANSFERASSETCONTRACT] =           [TransferAssetContract.deserializeBinary,'protocol.TransferAssetContract']
+ContractTable[ContractType.VOTEASSETCONTRACT] =               [VoteAssetContract.deserializeBinary,'protocol.VoteAssetContract']
+ContractTable[ContractType.VOTEWITNESSCONTRACT] =             [VoteWitnessContract.deserializeBinary,'protocol.VoteWitnessContract']
+ContractTable[ContractType.ASSETISSUECONTRACT] =              [AssetIssueContract.deserializeBinary,'protocol.AssetIssueContract']
+ContractTable[ContractType.DEPLOYCONTRACT] =                  [DeployContract.deserializeBinary,'protocol.DeployContract']
+ContractTable[ContractType.WITNESSUPDATECONTRACT] =           [WitnessUpdateContract.deserializeBinary,'protocol.WitnessUpdateContract']
+ContractTable[ContractType.PARTICIPATEASSETISSUECONTRACT] =   [ParticipateAssetIssueContract.deserializeBinary,'protocol.ParticipateAssetIssueContract']
+ContractTable[ContractType.FREEZEBALANCECONTRACT] =           [FreezeBalanceContract.deserializeBinary,'protocol.FreezeBalanceContract']
+ContractTable[ContractType.UNFREEZEBALANCECONTRACT] =         [UnfreezeBalanceContract.deserializeBinary,'protocol.UnfreezeBalanceContract']
+ContractTable[ContractType.WITHDRAWBALANCECONTRACT] =         [WithdrawBalanceContract.deserializeBinary,'protocol.WithdrawBalanceContract']
+ContractTable[ContractType.UNFREEZEASSETCONTRACT] =           [UnfreezeAssetContract.deserializeBinary,'protocol.UnfreezeAssetContract']
+ContractTable[ContractType.UPDATEASSETCONTRACT] =             [UpdateAssetContract.deserializeBinary,'protocol.UpdateAssetContract']
+/*not defined right now 
+  ContractTable[ContractType.CUSTOMCONTRACT] =                  [CustomContract.deserializeBinary,'protocol.CustomContract']
+*/
 
 function deserializeTransaction(tx) {
   try {
     const contractType = Transaction.Contract.ContractType;
-
     const contractList = tx.getRawData().getContractList();
-
     const transactions = [];
 
     contractList.forEach((contract) => {
       const any = contract.getParameter();
 
-      switch (contract.getType()) {
-        case contractType.ACCOUNTCREATECONTRACT: {
-        // contractType = contractType .ACCOUNTCREATECONTRACT;
+      const contractType = contract.getType();
+      let transaction = any.unpack(ContractTable[contractType][0], ContractTable[contractType][1]);
+      transaction = transaction.toObject();
+      //transaction.amount = obje.amount / 1000000; //TODO maybe remove in a future
 
-          const obje = any.unpack(AccountCreateContract.deserializeBinary, 'protocol.AccountCreateContract');
-
-          transactions.push({});
-        }
-          break;
-
-        default:
-        case contractType.TRANSFERCONTRACT: {
-        // let contractType = contractType .TRANSFERCONTRACT;
-
-          const obje = any.unpack(TransferContract.deserializeBinary, 'protocol.TransferContract');
-
-          const owner = obje.getOwnerAddress();
-          const ownerHex = getBase58CheckAddress(Array.from(owner));
-
-          const to = obje.getToAddress();
-          const toHex = getBase58CheckAddress(Array.from(to));
-
-          const amount = obje.getAmount() / 1000000;
-
-          const rawData = tx.getRawData();
-          const hash = byteArray2hexStr(SHA256(rawData.serializeBinary()));
-
-          transactions.push({
-            hash,
-            from: ownerHex,
-            to: toHex,
-            amount,
-            time: tx.getRawData().getTimestamp(),
-            data: rawData.getData(),
-            scripts: rawData.getScripts(),
-          });
-        }
-          break;
-      }
+      transaction.hash = byteArray2hexStr(SHA256(tx.getRawData().serializeBinary()))
+      transaction.time = tx.getRawData().getTimestamp();
+      transactions.push(transaction);
     });
 
     return transactions;
