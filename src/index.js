99a88e7cbd73e07d80a42894b8d8c9df5aaee387
Daniel Blanco Parla <dblancoparla@gmail.com>
2018-08-06 23:05:21 +0200

    Exported Account util

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const GrpcClient = require('./client/grpc');
 const SolidityGrpcClient = require('./client/solidity_grpc');
 const Transaction = require('./utils/transaction');
+const Account = require('./utils/account');
 
 module.exports = {
   GrpcClient,
   SolidityGrpcClient,
-  Transaction
+  Transaction,
+  Account,
 };
